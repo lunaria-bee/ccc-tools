875b0eeecfb693c5fd327802767c924f0926b437
Luna Peck <Luna.Peck@colorado.edu>
2023-10-18 15:07:20 -0600

    Added code for downloading repos

---
+++ defines.py
@@ -0,0 +1,11 @@
+'''Definitions used in other source files.'''
+
+
+from pathlib import Path
+
+
+REPOLIST_PATH = Path('./repolist.txt')
+'''Path to text file listing URLs of code repositories to download.'''
+
+REPODIR_PATH = Path('./repos')
+'''Path to the directory where code repositories are stored.'''
--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 #!/usr/bin/env python3
 
 
+from defines import REPOLIST_PATH
+from defines import REPODIR_PATH
+
+from git import Repo
+from pathlib import Path
+
+import logging
+import re
+import sys
+
+
+def download_repos():
+    logging.info("Retrieving repos...")
+
+    with open(REPOLIST_PATH) as repolist_file:
+        repolist_urls = [line.strip() for line in repolist_file.readlines()]
+
+    for repo_url in repolist_urls:
+        repo_name = re.fullmatch(r'.*/(.*?).git', repo_url).group(1)
+        repo_dir = REPODIR_PATH / Path(repo_name)
+        if repo_dir.exists():
+            logging.info(f"Skipping {repo_name}: Already downloaded.")
+        if not repo_dir.exists():
+            Repo.clone_from(repo_url, repo_dir)
+
+    logging.info("Finished retrieving repos.")
+
+
 def main(argv):
-    # TODO Get repos from file
+    logging.basicConfig(level=logging.INFO)
 
-    # TODO Download repos
+    download_repos()
 
-    # TODO Extract and annotate commit messages
+    # TODO Extract data
 
-    # TODO Extract and annotate comments
+    # TODO Annotate data
 
     pass
 
