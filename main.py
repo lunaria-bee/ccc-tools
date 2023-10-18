fc19cb8b36391fab2f90c9f8bf90dd0c8b5c5eb1
Luna Peck <Luna.Peck@colorado.edu>
2023-10-18 13:22:58 -0600

    Wrote outline of main function

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+def main(argv):
+    # TODO Get repos from file
+
+    # TODO Download repos
+
+    # TODO Extract and annotate commit messages
+
+    # TODO Extract and annotate comments
+
+    pass
+
+
+if __name__== '__main__': main(sys.argv)
