8ff4061d5c8480cb061fe64a699fe7fdbfa2ba11
Luna Peck <Luna.Peck@colorado.edu>
2023-10-19 12:00:45 -0600

    Added RepoManager class

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from defines import ITEM_END
 from defines import ITEM_START
 from defines import REPOLIST_PATH
 from defines import REPODIR_PATH
-from utils import get_repo_name_from_url
+from utils import RepoManager
 from utils import write_utterance_to_corpus_file
 
 from git import Repo
@@ -22,17 +22,9 @@ def download_repos():
 
     logging.info("Retrieving repos...")
 
-    with open(REPOLIST_PATH) as repolist_file:
-        repolist_urls = [line.strip() for line in repolist_file.readlines()]
-
-    for repo_url in repolist_urls:
-        repo_name = get_repo_name_from_url(repo_url)
-        repo_dir = REPODIR_PATH / Path(repo_name)
-        if repo_dir.exists():
-            logging.info(f"Skipping {repo_name}: Already downloaded.")
-        if not repo_dir.exists():
-            logging.info(f"Downloading {repo_name} from {repo_url}...")
-            Repo.clone_from(repo_url, repo_dir)
+    for repo in RepoManager.get_repolist():
+        logging.info(f" {repo.name}")
+        downloaded = repo.download()
 
     logging.info("Finished retrieving repos.")
 
@@ -42,25 +34,18 @@ def extract_data():
 
     logging.info("Extracting data...")
 
-    with open(REPOLIST_PATH) as repolist_file:
-        repolist_urls = [line.strip() for line in repolist_file.readlines()]
-
     CORPUSDIR_PATH.mkdir(exist_ok=True)
 
-    for repo_url in repolist_urls:
-        repo_name = get_repo_name_from_url(repo_url)
-        repo_dir = REPODIR_PATH / Path(repo_name)
-        commit_messages_path = CORPUSDIR_PATH / Path(f'commit_messages.{repo_name}.csv')
+    for repo in RepoManager.get_repolist():
+        commit_messages_path = CORPUSDIR_PATH / Path(f'commit_messages.{repo.name}.csv')
 
-        logging.info(f"From {repo_name}...")
-
-        repo = Repo(repo_dir)
+        logging.info(f" {repo.name}")
 
         # Write commits.
         with open(commit_messages_path, 'w') as commit_messages_file:
-            commit_count = len(list(repo.iter_commits()))
-            for i, commit in enumerate(repo.iter_commits()):
-                logging.debug(f"{repo_name} commit {i}/{commit_count}")
+            commit_count = len(list(repo.git.iter_commits()))
+            for i, commit in enumerate(repo.git.iter_commits()):
+                logging.debug(f"{repo.name} commit {i}/{commit_count}")
                 write_utterance_to_corpus_file(
                     commit_messages_file,
                     commit.message,
--- utils.py
+++ utils.py
@@ -4,20 +4,17 @@
 from defines import Category
 from defines import ITEM_END
 from defines import ITEM_START
+from defines import REPODIR_PATH
+from defines import REPOLIST_PATH
 
 from nltk import word_tokenize
+from pathlib import Path
 
+import git
+import logging
+import os
 import re
-
-
-def get_repo_name_from_url(url):
-    '''Use reposiroty URL to determine repository name.'''
-    match = re.fullmatch(r'.*/(.*?).git', url)
-
-    if match:
-        return match.group(1)
-    else:
-        raise ValueError(f"Could not interpret '{url}' as repository URL.")
+import shutil
 
 
 def write_line_to_corpus_file(
@@ -56,3 +53,97 @@ def write_utterance_to_corpus_file(
         category=Category.CONTROL,
         token=ITEM_END,
     )
+
+
+class RepoManager:
+    '''Manage data and git interactions for a repository.'''
+
+    def __init__(self, url):
+        self._url = url
+        self._name = RepoManager.get_name_from_url(url)
+        # self._commit # TODO
+        self._dir = REPODIR_PATH / Path(self._name)
+        self._git = None
+
+    def __str__(self):
+        return self._name
+
+    @staticmethod
+    def get_repolist(repolist_path=REPOLIST_PATH):
+        '''Get list of RepoManager objects created from repolist file.'''
+        with open(repolist_path) as repolist_file:
+            repolist = [RepoManager(url) for url in [line.strip() for line in repolist_file.readlines()]]
+
+        return repolist
+
+    @staticmethod
+    def get_name_from_url(url):
+        '''Use reposiroty URL to determine repository name.'''
+        match = re.fullmatch(r'.*/(.*?).git', url)
+
+        if match:
+            return match.group(1)
+        else:
+            raise ValueError(f"Could not interpret '{url}' as repository URL.")
+
+    def is_available(self):
+        '''Has the repository been downloaded?
+
+        Just checks that the path at self.dir is a directory with some contents; does not
+        check that those conetnts form a valid repository.
+
+        '''
+        return self._dir.is_dir() and os.listdir(self._dir)
+
+    def download(self, force_redownload=False):
+        '''Download the repository from data at its URL.
+
+        If the path at `self.dir` is a populated directory, this function assumes that the
+        repository has already been downloaded, ans skips it unless `force_redownload` is
+        `True`.
+
+        skip_redownload: Download repository even if directory at self.dir is populated.
+
+        '''
+        download = True
+        if self.is_available():
+            if force_redownload:
+                logging.debug(f"{self._name}: Forcing redownload")
+                shutil.rmtree(self._dir)
+                download = True
+            else:
+                logging.debug(f"{self._name}: Already downloaded")
+                download = False
+
+        if download:
+            logging.debug(f"{self._name}: Downloading...")
+            git.Repo.clone_from(self._url, self._dir)
+            logging.debug(f"{self._name}: Done.")
+
+        return download
+
+    @property
+    def url(self):
+        '''URL to download the repository from.'''
+        return self._url
+
+    @property
+    def name(self):
+        '''Name of the repository.'''
+        return self._name
+
+    @property
+    def dir(self):
+        '''Path to directory containing the repository.'''
+        return self._dir
+
+    @property
+    def git(self):
+        '''git.Repo object for version control interactions.'''
+        if self._git is None:
+            if not self.is_available():
+                self.download()
+
+            self._git = git.Repo(self._dir)
+
+        return self._git
