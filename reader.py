131b529f8e195cf38e5b6ad67e87e46d56d68869
Luna Peck <Luna.Peck@colorado.edu>
2023-11-09 21:31:04 -0700

    Override for CccReader.xml()

--- reader.py
+++ reader.py
@@ -6,6 +6,7 @@ from repo import RepoManager
 
 from nltk.corpus.reader.api import CategorizedCorpusReader
 from nltk.corpus.reader.xmldocs import XMLCorpusReader
+from xml.etree import ElementTree
 
 
 def get_fileid_components(fileid):
@@ -30,13 +31,13 @@ class CccReader(CategorizedCorpusReader, XMLCorpusReader):
     def fileids(self, categories=None, repos=None):
         '''Return a list of file identifiers for the files that make up this corpus.
 
-        May optionally filter to a subcorpus using the categories and repos
-        arguments. These arguments can be used together, returning the intersection of the
-        fileid lists that would be created by using them separately.
+        May optionally filter to a subcorpus using the categories and repos arguments. If
+        more than one of these parameters is set, only select fileids that satisfy all
+        provided criteria.
 
         categories: List of note categories (see defines.NoteType).
         repos: List of repositories. Each element may be either the repository name as a
-               string, or a RepoManager object.
+               string, or a RepoManager object. The list may contain a mixture of both.
 
         '''
         fileids = CategorizedCorpusReader.fileids(self, categories)
@@ -48,7 +49,36 @@ class CccReader(CategorizedCorpusReader, XMLCorpusReader):
 
         return fileids
 
-    # TODO override xml()
+    def xml(self, fileids=None, categories=None, repos=None):
+        '''Get Python representation of corpus XML.
+
+        Concatenate all XML from all corpus files, or those selected by the fileids,
+        categories, and repos arguments. If more than one of these parameters is set, only select
+        fileids that satisfy all provided criteria.
+
+        fileids: List of fileids.
+        categories: List of note categories (see defines.NoteType).
+        repos: List of repositories. Each element may be either the repository name as a
+        string, or a RepoManager object. The list may contain a mixture of both.
+
+        Return: xml.etree.ElementTree.Element tree representing the corpus.
+
+        '''
+        computed_fileids = self.fileids(categories, repos)
+
+        if fileids is None:
+            fileids = computed_fileids
+        else:
+            fileids = (fileid for fileid in fileids if fileid in computed_fileids)
+
+        xml_root = ElementTree.Element('notes')
+        for fileid in fileids:
+            file_notes = XMLCorpusReader.xml(self, fileid)
+            for note in file_notes:
+                xml_root.append(note)
+
+        return xml_root
+
     # TODO override words()
     # TODO override sents()
     # TODO delete paras()
